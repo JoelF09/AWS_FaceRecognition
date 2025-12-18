f9f2a4aecea7d778bbc2313389270e34910515c5
Joel Felix <Joel.Felix@edu.gbssg.ch>
2025-12-18 13:14:54 +0100

    Added echos for variables

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,3 +10,11 @@ OUT_BUCKET="${PREFIX}-out"
 ROLE_NAME="LabRole"          
 FUNCTION_NAME="${PREFIX}-facerecognition"
 
+echo "=== Using prefix: $PREFIX ==="
+echo "Region:   $REGION"
+echo "In-Bucket:  $IN_BUCKET"
+echo "Out-Bucket: $OUT_BUCKET"
+echo "Role:     $ROLE_NAME"
+echo "Function: $FUNCTION_NAME"
+echo "=============================="
+
